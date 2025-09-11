2d8400c8b0f41919a8b6ef3b42b3de40f232c55a
Pkto01 <theosuk01@gmail.com>
2025-09-11 16:55:18 +0200

    Update intcharacter.go

--- intcharacter.go
+++ intcharacter.go
@@ -1,12 +1,12 @@
 package main
 
-func initCharacter(Name string, Class string, Level int, Pvmax int, Pv int, Inventory []string) character {
-	return character{
-		Name:      "Your name",
-		Class:     "Elfe",
-		Level:     1,
-		Pvmax:     300,
-		Pv:        30,
-		Inventory: []string{"3 potions"},
+func initCharacter(Name string, Class string, Level int, Pvmax int, Pv int, Inventory []string) Character {
+	return Character{
+		Name:      Name,
+		Class:     Class,
+		Level:     Level,
+		Pvmax:     Pvmax,
+		Pv:        Pv,
+		Inventory: Inventory,
 	}
 }
