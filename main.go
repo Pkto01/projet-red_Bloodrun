5e0839a9d778240b03f94b016995b01804114f52
NyrZukA <master212rocma@gmail.com>
2025-09-12 14:38:50 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	displayInfo()
 	arthur := initCharacter("Arthur", "Barbare", 1, 100, 40, []string{"potion", "potion", "potion"})
 	displayInfo(arthur)
 }
